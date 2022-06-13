98b08b7a2177ed31f58d3523bba4962544109412
git <git@3712473.no-reply.drupal.org>
2022-06-13 06:43:04 -0400

    Issue #3285346 by Rakhi Soni, joecorall: phpcs --standard=Drupal AND phpcs --standard=Drupalpractice issues

--- src/Encoder/CslEncoder.php
+++ src/Encoder/CslEncoder.php
@@ -3,8 +3,6 @@
 namespace Drupal\islandora_csl\Encoder;
 
 use Symfony\Component\Serializer\Encoder\EncoderInterface;
-use Drupal\taxonomy\Entity\Term;
-use Drupal\node\Entity\Node;;
 
 /**
  * CSL format encoder.
@@ -25,25 +23,27 @@ class CslEncoder implements EncoderInterface {
     return $format === self::$format;
   }
 
-
   /**
    * {@inheritdoc}
    */
   public function encode($data, $format = '', array $context = []) {
     $result = [];
     foreach ($data as $field => $values) {
-      if (empty($values)) continue;
+      if (empty($values)) {
+        continue;
+      }
       switch ($field) {
         case 'title':
           $result['title'] = strip_tags($values[0]['value']);
           break;
+
         case 'field_linked_agent':
           foreach ($values as $value) {
-            $author = Term::load($value['target_id']);
+            $author = $this->entityTypeManager->getStorage('taxonomy_term')->load($value['target_id']);
             if ($author) {
               if ($author->vid->value == 'person') {
-                // assummes format GIVEN [MIDDLE] FAMILY
-                // TODO: improve
+                // Assummes format GIVEN [MIDDLE] FAMILY.
+                // @todo improve.
                 $components = explode(" ", $author->label());
                 $family = array_pop($components);
                 $given = implode(" ", $components);
@@ -60,18 +60,23 @@ class CslEncoder implements EncoderInterface {
             }
           }
           break;
+
         case 'field_publication':
-          $term = Term::load($values[0]['target_id']);
-          if ($term) $result['publisher'] = $term->label();
+          $term = $this->entityTypeManager->getStorage('taxonomy_term')->load($values[0]['target_id']);
+          if ($term) {
+            $result['publisher'] = $term->label();
+          }
           break;
+
         case 'field_edtf_date_issued':
         case 'field_edtf_date':
           $value = strip_tags($values[0]['value']);
           $result['issued']['date-parts'][] = explode('-', $value);
           break;
+
         case 'nid':
           $nid = $values[0]['value'];
-          $node = Node::load($nid);
+          $node = $this->nodeStorage->load($nid);
           if ($node) {
             $result['id'] = $nid;
             $result['URL'] = $node->toUrl('canonical', ['absolute' => TRUE])->toString();
@@ -82,4 +87,5 @@ class CslEncoder implements EncoderInterface {
 
     return json_encode($result);
   }
+
 }
